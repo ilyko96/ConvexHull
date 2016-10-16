892b74a3bb1d7d6e12b7ae2486dc1e04c2d091fb
Shrimp <Shrimp@ShrimProd>
2016-10-17 01:04:03 +0300

    Add project files.

---
+++ ArtyomsSolution/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtyomsSolution
+{
+    public abstract class Vershina
+    {
+        static protected Color col = Color.Aqua;
+        static protected int r = 50;
+        protected int x0;
+        protected int y0;
+        public int SETX { get { return x0; } set { x0 = value; } }
+        public int SETY { get { return y0; } set { y0 = value; } }
+
+        public Vershina()
+        { x0 = y0 = 100; }
+
+        public Vershina(int x, int y)
+        {
+            this.x0 = x;
+            this.y0 = y;
+        }
+
+        static Vershina()
+        {
+            r = 50;
+            col = Color.Aqua;
+        }
+
+        public abstract void DrawFigure(Graphics g);
+        abstract public bool Check(int x, int y);
+    }
+
+    public class Krug : Vershina
+    {
+        public Krug(int x, int y) : base(x, y) { }
+        public override void DrawFigure(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(col), x0 - r / 2, y0 - r / 2, r, r);
+        }
+
+        override public bool Check(int x, int y)
+        {
+            if (Math.Sqrt((Math.Pow(x - x0, 2)) + (Math.Pow(y - y0, 2))) <= r)
+                return true;
+            return false;
+        }
+    }
+
+    public class Kvadrat : Vershina
+    {
+        public Kvadrat(int x, int y) : base(x, y) { }
+        public override void DrawFigure(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(col), x0 - r / 2, y0 - r / 2, r, r);
+        }
+
+        override public bool Check(int x, int y)
+        {
+            if (Math.Abs(x - x0) <= r / 2 && (Math.Abs(y - y0) <= r / 2))
+                return true;
+            return false;
+        }
+    }
+
+    public class Treygolnik : Vershina
+    {
+        public Treygolnik(int x, int y) : base(x, y) { }
+        public override void DrawFigure(Graphics g)
+        {
+            PointF[] triangle = new PointF[] { new PointF(x0 - r/2, y0 + (float)Math .Sqrt(3)*r/6),
+                            new PointF(x0 + r / 2, y0 + (float)Math.Sqrt(3) * r / 6),
+                            new PointF(x0, y0 - (float)Math.Sqrt(3) * r / 3) };
+            g.FillPolygon(new SolidBrush(col), triangle);
+        }
+
+        override public bool Check(int x, int y)
+        {
+            float l1, l2, l3, p1, p2, p3, p, S1, S2, S3, S;
+            l1 = (float)Math.Sqrt((-x + (x0 - r / 2)) * (-x + (x0 - r / 2)) + (-y + (y0 + (float)Math.Sqrt(3) * r / 6) - Math.Sqrt(3) * r / 3) * (-y + ((y0 + (float)Math.Sqrt(3) * r / 6)) - Math.Sqrt(3) * r / 3));
+            l2 = (float)Math.Sqrt(((x0 + r / 2) - r / 2 - x) * ((x0 + r / 2) - r / 2 - x) + ((y0 + (float)Math.Sqrt(3) * r / 6) + Math.Sqrt(3) * r / 6 - y)
+                            * ((y0 + (float)Math.Sqrt(3) * r / 6) + Math.Sqrt(3) * r / 6 - y));
+            l3 = (float)Math.Sqrt((x0 + r / 2 - x) * (x0 + r / 2 - x) + ((y0 - (float)Math.Sqrt(3) * r / 3) - y + Math.Sqrt(3) * r / 6) * ((y0 - (float)Math.Sqrt(3) * r / 3) - y + Math.Sqrt(3) * r / 6));
+            p1 = (r + l1 + l2) / 2;
+            p2 = (l2 + l3 + r) / 2;
+            p3 = (l1 + l3 + r) / 2;
+            p = (r + r + r) / 2;
+            S1 = (float)Math.Sqrt(p1 * (p1 - l1) * (p1 - l2) * (p1 - r));
+            S2 = (float)Math.Sqrt(p2 * (p2 - l3) * (p2 - l2) * (p2 - r));
+            S3 = (float)Math.Sqrt(p3 * (p3 - l1) * (p3 - l3) * (p3 - r));
+            S = (float)Math.Sqrt(p * (p - r) * (p - r) * (p - r));
+            if (S == S1 + S2 + S3)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ ArtyomsSolution/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ArtyomsSolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(116, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 21);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "checkBox1";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(220, 12);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(98, 21);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "checkBox1";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 349);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
